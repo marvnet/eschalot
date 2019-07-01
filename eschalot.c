30fdda753d5d29a6952cf765713205d4f48ad77b
Marvin <24806960+MagicMarvMan@users.noreply.github.com>
2019-07-01 13:14:02 +0300

    Add color definitions

--- eschalot.c
+++ eschalot.c
@@ -111,6 +111,16 @@ strnlen(const char *s, size_t ml)
 #define MAX_WORDS	0xFFFFFFFFu		/* Maximum words to read from file */
 #define BASE32_ALPHABET	"abcdefghijklmnopqrstuvwxyz234567"
 
+/* define colors for more beautiful console output */
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+
 extern char	*__progname;
 
 /* Error and debug functions */
